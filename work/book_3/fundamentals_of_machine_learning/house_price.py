83cbc577da3aaf05517d0b1189f02dc21fa0d7d3
Mithiriii <74918078+Mithiriii@users.noreply.github.com>
2022-02-17 15:53:02 +0100

    book3

---
+++ work/book_3/fundamentals_of_machine_learning/house_price.py
@@ -0,0 +1,139 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.impute import SimpleImputer
+
+
+#############################################################POBIERANIE DANYCH#############################################################
+#ścieżka do danych
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+
+    #ładowanie pliku csv
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+housing = load_housing_data()
+#housing.hist(bins=50, figsize=(20, 15))
+#plt.show()
+
+
+#############################################################DZIELENIE ZBIORU NA TESTOWY I TRENINGOWY#############################################################
+    #dzielenie zbioru w celu stworzenia zbioru testowego
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+#train_set, test_set = split_train_test(housing, 0.2)
+
+    #takie rozwiązanie wiąże się z problemami:
+    #przy kolejnych uruchomieniach programu zostanie wygenerowany inny zbiór testowy
+    #rozwiązania:
+    #zapisanie zestawu i wczytywanie go przy kolejnych okazjach
+    #użycie seed(x) w celu wygenerowania takiego samego seedu dla każdego uruchomienia programu
+
+    #oba te rozwiązania zawiodą gdy dostarczymy nowe dane, należy użyć funkcji hashującej
+
+
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+    #do hashowania należy używać stabilnych danych
+    #w tym wypadku jako, że nie mamy id dobre będą współrzędne geograficzne
+
+
+housing_with_id = housing.reset_index()     #dodaje kolumne index [id]
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+#train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+    #takie rozwiązanie przy dodaniu nowych danych przeniesie 20% z nich do starego zbioru treningowego a stary zbiór danych zostanie nienaruszony
+    #jednak losowe próbkowanie spisuje się dobrze w wypadku dużych zbiorów danych, inaczej ryzykujemy obciążenie próbkowania
+    #np. jeżeli A stanowi 73%B i 27%C to podobna ilość przykładów powinna pochodzi z B i C, w losowym często tak nie będzie
+    #należy użyć losowania warstwowego
+
+
+    #wyznaczamy kategorię dochodów w celu użycia losowania warstwowego
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+    #kod generujący atrybut kategorii dochodów poprzez podzielanie wartości mediany przez 1,5
+    #i zaokrąglenie wyników za pomocą funkcji ceil (aby wyodrębnić poszczególne kategorie)
+
+#housing["income_cat"].hist()
+#plt.show()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    start_train_set = housing.loc[train_index]
+    start_test_set = housing.loc[test_index]
+
+    #usunięcie atrybutu income_cat dzięki czemu dane powracają do pierwotnego stanu
+for set_ in (start_train_set, start_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+#############################################################WIZUALIZACJA I ODKRYWANIE DANCYH W CELU ZDOBYWANIA NOWYCH INFORMACJI#############################################################
+housing = start_train_set.copy()
+#housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+#             s=housing["population"]/100, label="Population", figsize=(10, 7),
+#             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+#             )
+#plt.legend()
+#plt.show()
+
+
+#wyliczenie korelacji liniowej
+#corr_matrix = housing.corr()
+
+#print(corr_matrix["median_house_value"].sort_values(ascending=False))
+    #przed przygotwaniem danych do użytku ml należy wypróbowac różne kombinacje atrybutów
+    #np. pokoje na rodzine, sypialnie na rodzine, populacja na rodzinę i zobaczyć jak odnosza się one do koleracji z ceną mieszkań
+
+housing["rooms_to_families"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_to_rooms"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_to_family"] = housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+#print(corr_matrix["median_house_value"].sort_values(ascending=False))
+    #sypialnie na pokoje okazały się bardziej skorelowane z medianą cen mieszkań niż liczba pomieszczeń czy liczba sypialni osobno
+    #mieszkania o mniejszym współczynniku liczby sypialni do liczby pomieszczeń okazują się droższe
+    #liczba pokojów na rodzinę również ma korelację, wraz z powierzchnią domu rośnie jego cena
+    #wyłapywanie takich korelacji pozwoli zbudować lepszy model
+
+#############################################################PRZYGOTYWYWANIE DANYCH POD ML#############################################################
+
+    #rozdzielamy czynniki prognostyczne od etykiet
+housing = start_train_set.drop("median_house_value", axis=1)        #funkcja dorp tworzy kopie danych
+housing_labels = start_train_set["median_house_value"].copy()
+
+#w atrybucie total_bedrooms brakuje kilku wartości
+#są trzy możliwości rozwiązania tego problemu
+#-pozbyć się dystryktów zawierających brakujące dane
+#-pozbyć się atrybutu
+#-uzupełnić dane określoną wartością (zero, średnia, mediana itd.)
+
+#housing.dropna(subset=["total_bedrooms"])       #opcja 1
+#housing.drop("total_bedrooms", axis=1)          #opcja 2
+#median = housing["total_bedrooms"].median()     #opcja 3
+#housing["total_bedrooms"].fillna(median, inplace=True)
+
+#z modułu scki-learn można użyć modułu simpleimputer zajmujący się brakującymi wartościami
+imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+housing_num = housing.drop("ocean_proximity", axis=1) #ocen_proximity nie jest wartością numeryczną
+imp_mean.fit(housing_num)
+#najbezpieczniej będzie użyć tego do wszystkich wartości numerycznych
+X = imp_mean.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
